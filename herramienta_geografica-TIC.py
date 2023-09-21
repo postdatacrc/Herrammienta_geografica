82d2f2752ab3b0bd5b9cc9b98cfa385719c57302
Comisión de regulación de comunicaciones <95722715+postdatacrc@users.noreply.github.com>
2023-09-21 10:22:32 -0500

    Add files via upload

--- herramienta_geografica-TIC.py
+++ herramienta_geografica-TIC.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
-import matplotlib.pyplot as plt
 import streamlit as st
 from plotly.subplots import make_subplots
 import re
